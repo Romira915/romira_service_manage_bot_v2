54b23a7117decd17cf0ab5bb82913a666b242545
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-10-28 23:59:46 +0900

    wip

---
+++ wakaba_game_service_manage_api/src/domain.rs
@@ -0,0 +1,26 @@
+use crate::SystemdControl;
+use schema::SystemdCommand;
+
+pub(crate) async  fn sdtd_systemd_executable(
+    sdtd_systemd: impl SystemdControl,
+    command: SystemdCommand,
+) -> anyhow::Result<bool> {
+    match command {
+        SystemdCommand::Start => {
+            sdtd_systemd.start().await?;
+        }
+        SystemdCommand::Stop => {
+            sdtd_systemd.stop().await?;
+        }
+        SystemdCommand::Restart => {
+            sdtd_systemd.restart().await?;
+        }
+        SystemdCommand::IsActive => {
+            let is_active = sdtd_systemd.is_active().await?;
+            if !is_active {
+                return Ok(false);
+            }
+        }
+    }
+    Ok(true)
+}
