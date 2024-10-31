9fc05c4d0a4a6611e69b6bbcda11348fce859c73
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-11-01 00:20:27 +0900

    fix: unauthorized response

--- wakaba_game_service_manage_api/src/custom_middleware.rs
+++ wakaba_game_service_manage_api/src/custom_middleware.rs
@@ -7,13 +7,13 @@ use axum::response::IntoResponse;
 pub(super) async fn require_auth(
     request: Request,
     next: Next,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> Result<impl IntoResponse, impl IntoResponse> {
     let auth_header = request.headers().get("Authorization");
 
     match auth_header {
         Some(value) if value == &format!("Bearer {}", CONFIG.bearer_token) => {
             Ok(next.run(request).await)
         }
-        _ => Err(StatusCode::UNAUTHORIZED),
+        _ => Err((StatusCode::UNAUTHORIZED, "Unauthorized: missing or invalid Authorization header")),
     }
 }
